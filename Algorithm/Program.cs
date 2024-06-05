f8b6d12313bc52c08b2d28b8fd28583cf07b4bd3
samura <samura@DESKTOP-66K9F5E>
2024-06-05 12:51:53 +0300

    Add project files.

---
+++ Algorithm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] coinDenoms = {1,2,5,10,20,50,100};
+        int[] coinValues = { 1, 2, 5, 10, 20, 50, 100 };
+        int[] coinCount= new int[coinDenoms.Length];
+
+        int amount = 36;
+        int[] result= new int[coinDenoms.Length];
+
+        for (int i=coinDenoms.Length - 1; i>=0; i--)
+        {
+            int count = amount / coinDenoms[i];
+            amount -= count * coinDenoms[i];
+            result[i] += count;
+        }
+        Console.WriteLine("Minimum number of coins needed: " + string.Join(",", result));
+
+    }
+}
